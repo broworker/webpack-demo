2daf79578d2c478a485c9aa637e24c2725c7df9e
broworker <broworker@gmail.com>
2016-05-15 19:40:05 +0800

    git init

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+// require('normalize.css/normalize.css');
+// require('styles/App.css');
+
+import React from 'react';
+import TextInput from './TextInput';
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>This is the App Component</h1>
+        <TextInput/>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
